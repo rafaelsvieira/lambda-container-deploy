ffb7944aa52c7b0ba6550f29fa76b0c1c4a2808b
rafaelsvieira <rafael.souzavieira@gmail.com>
2024-10-25 15:41:41 -0300

    feat(infra): Add outputs to be used in the workflow

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -2,3 +2,13 @@ output "codedeploy_app_name" {
   description = "Name of CodeDeploy application"
   value       = module.deploy.codedeploy_app_name
 }
+
+output "lambda_alias_name" {
+  description = "Lambda alias name"
+  value       = module.lambda_alias.lambda_alias_name
+}
+
+output "lambda_function_name" {
+  description = "lambda function name"
+  value       = module.lambda_function.lambda_function_name
+}
