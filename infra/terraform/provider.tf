839251486814dd123900555cd44d054ce9a7079c
rafaelsvieira <rafael.souzavieira@gmail.com>
2024-10-27 17:51:10 -0300

    feat(infra): Add bucket to store the statefile and set backend TF

--- infra/terraform/provider.tf
+++ infra/terraform/provider.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.73.0"
+    }
+  }
+  backend "s3" {
+    bucket = "rafaelsvieira-my-bucket"
+    key    = "terraform/statefile/lambda-container-deploy"
+    region = "us-east-1"
+  }
+}
